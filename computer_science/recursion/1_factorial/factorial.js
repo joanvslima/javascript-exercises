5b3d1ae06c903b275266a4e1ac6defc543f11896
Joan Lima <joanlima2010@gmail.com>
2026-03-03 22:44:24 -0300

    factorial exercise resolution

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,14 @@
-const factorial = function() {
-
+const factorial = function(n) {
+  if (n < 0) {
+    return
+  } else if (n == 1) {
+    return 1
+  } else {
+    return n * factorial(n - 1)
+  }
 };
+
+console.log(factorial(-1));
 
 // Do not edit below this line
 module.exports = factorial;
